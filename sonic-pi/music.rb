7518f792874d4c04b2073cdc489f2ba2e880a49e
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-02-13 18:44:10 +0800

    Colours!

--- sonic-pi/music.rb
+++ sonic-pi/music.rb
@@ -16,111 +16,119 @@ SFX:
 
 =end
 
-x = 20
+x = 100
+b =100
 
-require 'pusher-client'
-cluster = 'us2'  
-channels_client = PusherClient::Socket.new('b0fffd0f6692eadd6d60', {
-  secure: true,
-  ws_host: "ws-#{cluster}.pusher.com"
-})
+live_loop :midi_sync do
+  q, a = sync "/midi:my_virtual_output:1/note_on"
+  if(a == 1)
+    b = q
+  else
+    x = q
+  end
+end
 
 # Spooky Background Mood
-
-live_loop :haunted do
-  if(x==0)
-    sample :perc_bell, rate: rrand(-1.5, 1.5)
-  end
-  sleep rrand(0.1, 2)
-end
-live_loop :guit do
-  if(x==0)
-    with_fx :echo, mix: 0.3, phase: 0.25 do
-      sample :guit_em9, rate: 0.5
+with_fx :band_eq, amp:0.1 do
+  live_loop :haunted do
+    if(x==0)
+      sample :perc_bell, rate: rrand(1, 1.5)
     end
-    sample :guit_em9, rate: 1.5
+    sleep rrand(5, 7)
   end
-  sleep 8
-end
-live_loop :boom do
-  if(x==0)
-    with_fx :reverb, room: 1 do
-      sample :bd_boom, amp: 10, rate: 1
+  live_loop :guit do
+    if(x==0)
+      with_fx :echo, mix: 0.3, phase: 0.25 do
+        sample :guit_em9, rate: 0.5
+      end
+      sample :guit_em9, rate: 1.5
+      sleep 4
     end
+    sleep 0.1
   end
-  sleep 8
+  live_loop :boom do
+    if(x==0)
+      with_fx :reverb, room: 1 do
+        sample :bd_boom, amp: 10, rate: 1
+      end
+      sleep 4
+    end
+    sleep 0.1
+  end
 end
 
 # Electric Mood
 
 load_samples :guit_em9, :bd_haus
-live_loop :low do
-  if(x==1)
-    tick
-    synth :zawa, wave: 1, phase: 0.25, release: 5, note: (knit :e1, 12, :c1, 4).look, cutoff: (line 60, 120, steps: 6).look
-  end
-  sleep 4
-end
-with_fx :reverb, room: 1 do
-  live_loop :lands, auto_cue: false do
+with_fx :band_eq, amp:0.02 do
+  live_loop :low do
     if(x==1)
-      use_synth :dsaw
-      use_random_seed 310003
-      ns = (scale :e2, :minor_pentatonic, num_octaves: 4).take(8)
-      16.times do
-        play ns.choose, detune: 12, release: 0.1, amp: 2, amp: rand + 0.5, cutoff: rrand(70, 120), amp: 2
-        sleep 0.125
-      end
-    end
-    sleep 0.125
-  end
-end
-live_loop :fietsen do
-  sleep 0.25
-  if(x==1)
-    sample :guit_em9, rate: 1
-  end
-  sleep 7.75
-end
-live_loop :tijd do
-  if(x==1)
-    sample :bd_haus, amp: 2.5, cutoff: 100
-  end
-  sleep 1
-end
-live_loop :ind do
-  if(x==1)
-    sample :loop_industrial, beat_stretch: 1
-  end
-  sleep 1
-end
-
-# Undefined
-
-live_loop :synths do
-  use_synth :mod_saw
-  use_synth_defaults amp: 0.5, attack: 0, sustain: 1, release: 0.25, mod_range: 12, mod_phase: 0.5, mod_invert_wave: 1
-  notes = (ring :F, :E, :D, :D, :G, :E, :D, :D)
-  notes.each do |n|
-    if(x==2)
       tick
-      play note(n, octave: 1), cutoff: (line 90, 130, steps: 8).look
-      play note(n, octave: 2), cutoff: (line 90, 130, steps: 16).look
-      
+      synth :zawa, wave: 1, phase: 0.25, release: 5, note: (knit :e1, 12, :c1, 4).look, cutoff: (line 60, 120, steps: 6).look
+    end
+    sleep 4
+  end
+  with_fx :reverb, room: 1 do
+    live_loop :lands, auto_cue: false do
+      if(x==1)
+        use_synth :dsaw
+        use_random_seed 310003
+        ns = (scale :e2, :minor_pentatonic, num_octaves: 4).take(8)
+        16.times do
+          play ns.choose, detune: 12, release: 0.1, amp: 2, amp: rand + 0.5, cutoff: rrand(70, 120), amp: 2
+          sleep 0.125
+        end
+      end
+      sleep 0.125
+    end
+  end
+  live_loop :fietsen do
+    sleep 0.25
+    if(x==1)
+      sample :guit_em9, rate: 1
+    end
+    sleep 7.75
+  end
+  live_loop :tijd do
+    if(x==1)
+      sample :bd_haus, amp: 2.5, cutoff: 100
+    end
+    sleep 1
+  end
+  live_loop :ind do
+    if(x==1)
+      sample :loop_industrial, beat_stretch: 1
     end
     sleep 1
   end
 end
-live_loop :snare, delay: 12.5 do
-  if(x==2)
-    sample :drum_snare_soft
+
+
+# Undefined
+with_fx :band_eq, amp:0.05 do
+  live_loop :synths do
+    use_synth :mod_saw
+    use_synth_defaults amp: 0.5, attack: 0, sustain: 1, release: 0.25, mod_range: 12, mod_phase: 0.5, mod_invert_wave: 1
+    notes = (ring :F, :E, :D, :D, :G, :E, :D, :D)
+    notes.each do |n|
+      if(x==2)
+        tick
+        play note(n, octave: 1), cutoff: (line 90, 130, steps: 8).look
+        play note(n, octave: 2), cutoff: (line 90, 130, steps: 16).look
+        
+      end
+      sleep 1
+    end
   end
-  sleep 1
+  live_loop :snare, delay: 12.5 do
+    if(x==2)
+      sample :drum_snare_soft
+    end
+    sleep 1
+  end
 end
 
 # SFX Zone
-
-b=10
 
 live_loop :effects do
   if(b == 0)
--- web/pages/index.js
+++ web/pages/index.js
@@ -4,8 +4,21 @@ import React, { useState } from "react";
 import { useChannel, useEvent } from "@harelpls/use-pusher";
 
 export default function Home() {
-  const beatColours = { 0: "#eb6123" };
-  const sfxColours = { 0: "#eb6123" };
+  const beatColours = { 0: "#eb6123", 1: "#a633d6", 2: "#33d6a6" };
+  const sfxColours = {
+    0: "#5bc0de",
+    1: "#ec3750",
+    2: "#a633d6",
+    3: "#ec3750",
+    4: "#ff8c37",
+    5: "#f1c40f",
+    6: "#a633d6",
+    7: "#33d6a6",
+    8: "#338eda",
+    9: "#a633d6",
+    10: '#5bc0de',
+    11: '#338eda'
+  };
   const [colour, setColour] = useState("#000");
   const channel = useChannel("sound-festival");
   function sleep(ms) {
